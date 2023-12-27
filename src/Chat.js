5945f9a8a55444a1835ebd2faed86b949b359124
harshblip <harsh.jipkate2020@vitbhopal.ac.in>
2023-12-27 11:56:17 +0530

    voice record and replay working

--- src/Chat.js
+++ src/Chat.js
@@ -1,56 +1,28 @@
-import React, { useState } from 'react';
-import { useSpeechRecognition } from 'react-speech-recognition';
-import { CometChat } from "@cometchat-pro/chat";
-import { useVoiceVisualizer } from 'react-voice-visualizer';
-import AudioPlayer from 'react-h5-audio-player';
-import convertTranscriptToAudioFile from 'react-h5-audio-player';
-import 'react-h5-audio-player/lib/styles.css';
+import React from "react";
+import { AudioRecorder, audioTrackConstraints } from 'react-audio-voice-recorder';
+import { onNotAllowedOrFound } from'react-audio-voice-recorder';
+
+const addAudioElement = (blob) => {
+  const url = URL.createObjectURL(blob);
+  const audio = document.createElement("audio");
+  audio.src = url;
+  audio.controls = true;
+  document.body.appendChild(audio);
+};
 
 const Chat = () => {
-  const [isListening, setIsListening] = useState(false);
-  const { transcript, resetTranscript } = useSpeechRecognition();
-  const recorderControls = useVoiceVisualizer();
-  const [audioFile, setAudioFile] = useState(null);
-
-  const startListening = () => {
-    setIsListening(true);
-    recorderControls.startRecording();
-  };
-
-  const stopListening = () => {
-    setIsListening(false);
-    recorderControls.stopRecording();
-  };
-
-  // Send the transcript as a message through CometChat
-  const sendMessage = () => {
-    const textMessage = new CometChat.TextMessage(
-      'uid',
-      transcript,
-      CometChat.MESSAGE_TYPE.TEXT,
-      CometChat.RECEIVER_TYPE.USER
-    );
-    CometChat.sendMessage(textMessage);
-    const audioFile = convertTranscriptToAudioFile(transcript);
-    setAudioFile(audioFile);
-  };
-
   return (
-    <div className='App'>
-      {isListening ? (
-        <button onClick={stopListening}>Stop</button>
-      ) : (
-        <button onClick={startListening}>Start</button>
-      )}
-      <button onClick={sendMessage}>Send</button>
-      <p>{transcript}</p>
-      {audioFile && (
-        <AudioPlayer
-          autoPlayAfterSrcChange={false}
-          src={audioFile}
-          customAdditionalControls={[]}
-        />
-      )}
+    <div>
+      <AudioRecorder
+        onRecordingComplete={addAudioElement}
+        audioTrackConstraints={{
+          noiseSuppression: true,
+          echoCancellation: true,
+        }}
+        showVisualizer={true}
+        downloadFileExtension="webm"
+        classes={'audiorecorderr'}
+      />
     </div>
   );
 };
